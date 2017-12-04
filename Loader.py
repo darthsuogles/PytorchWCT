1ca1ec7d7f2c8ab0fb8fc39d9aa2af5c89f97ab2
Philip Yang <phissenschaft@gmail.com>
2017-12-03 21:08:39 -0800

    more readable + one image with multi-style

--- Loader.py
+++ Loader.py
@@ -17,49 +17,53 @@ def is_image_file(filename):
 def default_loader(path):
     return Image.open(path).convert('RGB')
 
-class Dataset(data.Dataset):
-    def __init__(self,contentPath,stylePath,fineSize):
-        super(Dataset,self).__init__()
-        self.contentPath = contentPath
-        self.image_list = [x for x in listdir(contentPath) if is_image_file(x)]
-        self.stylePath = stylePath
-        self.fineSize = fineSize
+class MultiStyleDataset(data.Dataset):
+    def __init__(self, content_fname, styles_fpath, fine_size):
+        super(MultiStyleDataset, self).__init__()
+        assert is_image_file(content_fname), \
+            '{} must be an image file'.format(content_fname)
+        self.style_images_basedir = styles_fpath
+        self.content_fname = content_fname
+        self.style_image_fnames = [
+            fname for fname in listdir(styles_fpath) if is_image_file(fname)]
+        self.fine_size = fine_size
         #self.normalize = transforms.Normalize(mean=[103.939,116.779,123.68],std=[1, 1, 1])
         #normalize = transforms.Normalize(mean=[123.68,103.939,116.779],std=[1, 1, 1])
         self.prep = transforms.Compose([
-                    transforms.Scale(fineSize),
+                    transforms.Scale(fine_size),
                     transforms.ToTensor(),
                     #transforms.Lambda(lambda x: x[torch.LongTensor([2,1,0])]), #turn to BGR
                     ])
 
-    def __getitem__(self,index):
-        contentImgPath = os.path.join(self.contentPath,self.image_list[index])
-        styleImgPath = os.path.join(self.stylePath,self.image_list[index])
-        contentImg = default_loader(contentImgPath)
+    def __getitem__(self, index):
+        styleImgPath = os.path.join(self.style_images_basedir, self.style_image_fnames[index])
+        contentImg = default_loader(self.content_fname)
         styleImg = default_loader(styleImgPath)
 
         # resize
-        if(self.fineSize != 0):
-            w,h = contentImg.size
+        if self.fine_size != 0:
+            w, h = contentImg.size
             if(w > h):
-                if(w != self.fineSize):
-                    neww = self.fineSize
-                    newh = h*neww/w
-                    contentImg = contentImg.resize((neww,newh))
-                    styleImg = styleImg.resize((neww,newh))
+                if w != self.fine_size:
+                    neww = self.fine_size
+                    newh = h * neww // w
+                    contentImg = contentImg.resize((neww, newh))
+                    styleImg = styleImg.resize((neww, newh))
             else:
-                if(h != self.fineSize):
-                    newh = self.fineSize
-                    neww = w*newh/h
-                    contentImg = contentImg.resize((neww,newh))
-                    styleImg = styleImg.resize((neww,newh))
+                if h != self.fine_size:
+                    newh = self.fine_size
+                    neww = w * newh // h
+                    contentImg = contentImg.resize((neww, newh))
+                    styleImg = styleImg.resize((neww, newh))
 
 
         # Preprocess Images
         contentImg = transforms.ToTensor()(contentImg)
         styleImg = transforms.ToTensor()(styleImg)
-        return contentImg.squeeze(0),styleImg.squeeze(0),self.image_list[index]
+        _out_fname_elems = self.content_fname.split('/')[-1].split('.')[:-1]
+        out_fname = '{}_style_{}.jpg'.format('.'.join(_out_fname_elems), index)
+        return contentImg.squeeze(0), styleImg.squeeze(0), out_fname
 
     def __len__(self):
         # You should change 0 to the total size of your dataset.
-        return len(self.image_list)
+        return len(self.style_image_fnames)
--- WCT.py
+++ WCT.py
@@ -6,31 +6,81 @@ from torch.autograd import Variable
 import torchvision.utils as vutils
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
-from Loader import Dataset
+from Loader import MultiStyleDataset
 from util import *
 import scipy.misc
 from torch.utils.serialization import load_lua
 import time
 
 parser = argparse.ArgumentParser(description='WCT Pytorch')
-parser.add_argument('--contentPath',default='images/content',help='path to train')
-parser.add_argument('--stylePath',default='images/style',help='path to train')
-parser.add_argument('--workers', default=2, type=int, metavar='N',help='number of data loading workers (default: 4)')
-parser.add_argument('--vgg1', default='models/vgg_normalised_conv1_1.t7', help='Path to the VGG conv1_1')
-parser.add_argument('--vgg2', default='models/vgg_normalised_conv2_1.t7', help='Path to the VGG conv2_1')
-parser.add_argument('--vgg3', default='models/vgg_normalised_conv3_1.t7', help='Path to the VGG conv3_1')
-parser.add_argument('--vgg4', default='models/vgg_normalised_conv4_1.t7', help='Path to the VGG conv4_1')
-parser.add_argument('--vgg5', default='models/vgg_normalised_conv5_1.t7', help='Path to the VGG conv5_1')
-parser.add_argument('--decoder5', default='models/feature_invertor_conv5_1.t7', help='Path to the decoder5')
-parser.add_argument('--decoder4', default='models/feature_invertor_conv4_1.t7', help='Path to the decoder4')
-parser.add_argument('--decoder3', default='models/feature_invertor_conv3_1.t7', help='Path to the decoder3')
-parser.add_argument('--decoder2', default='models/feature_invertor_conv2_1.t7', help='Path to the decoder2')
-parser.add_argument('--decoder1', default='models/feature_invertor_conv1_1.t7', help='Path to the decoder1')
+parser.add_argument(
+    '--contentImageFile',
+    default='images/content/la_chatte.png',
+    help='path to train')
+parser.add_argument(
+    '--stylePath',
+    default='images/style',
+    help='path to train')
+parser.add_argument(
+    '--workers',
+    default=2,
+    type=int,
+    metavar='N',
+    help='number of data loading workers (default: 4)')
+parser.add_argument(
+    '--vgg1',
+    default='models/vgg_normalised_conv1_1.t7',
+    help='Path to the VGG conv1_1')
+parser.add_argument(
+    '--vgg2',
+    default='models/vgg_normalised_conv2_1.t7',
+    help='Path to the VGG conv2_1')
+parser.add_argument(
+    '--vgg3',
+    default='models/vgg_normalised_conv3_1.t7',
+    help='Path to the VGG conv3_1')
+parser.add_argument(
+    '--vgg4',
+    default='models/vgg_normalised_conv4_1.t7',
+    help='Path to the VGG conv4_1')
+parser.add_argument(
+    '--vgg5',
+    default='models/vgg_normalised_conv5_1.t7',
+    help='Path to the VGG conv5_1')
+parser.add_argument(
+    '--decoder5',
+    default='models/feature_invertor_conv5_1.t7',
+    help='Path to the decoder5')
+parser.add_argument(
+    '--decoder4',
+    default='models/feature_invertor_conv4_1.t7',
+    help='Path to the decoder4')
+parser.add_argument(
+    '--decoder3',
+    default='models/feature_invertor_conv3_1.t7',
+    help='Path to the decoder3')
+parser.add_argument(
+    '--decoder2',
+    default='models/feature_invertor_conv2_1.t7',
+    help='Path to the decoder2')
+parser.add_argument(
+    '--decoder1',
+    default='models/feature_invertor_conv1_1.t7',
+    help='Path to the decoder1')
 parser.add_argument('--cuda', action='store_true', help='enables cuda')
 parser.add_argument('--batch_size', type=int, default=1, help='batch size')
-parser.add_argument('--fineSize', type=int, default=512, help='resize image to fineSize x fineSize,leave it to 0 if not resize')
-parser.add_argument('--outf', default='samples/', help='folder to output images')
-parser.add_argument('--alpha', type=float,default=1, help='hyperparameter to blend wct feature and content feature')
+parser.add_argument(
+    '--fineSize',
+    type=int,
+    default=512,
+    help='resize image to fineSize x fineSize,leave it to 0 if not resize')
+parser.add_argument(
+    '--outf', default='samples/', help='folder to output images')
+parser.add_argument(
+    '--alpha',
+    type=float,
+    default=1,
+    help='hyperparameter to blend wct feature and content feature')
 args = parser.parse_args()
 
 try:
@@ -39,27 +89,31 @@ except OSError:
     pass
 
 # Data loading code
-dataset = Dataset(args.contentPath,args.stylePath,args.fineSize)
-loader = torch.utils.data.DataLoader(dataset=dataset,
-                                     batch_size=1,
-                                     shuffle=False)
-def scale_dialate(seg,W,H):
+#dataset = Dataset(args.contentPath, args.stylePath, args.fineSize)
+dataset = MultiStyleDataset(args.contentImageFile, args.stylePath, args.fineSize)
+loader = torch.utils.data.DataLoader(
+    dataset=dataset, batch_size=1, shuffle=False)
+
+
+def scale_dialate(seg, W, H):
     # TODO: dialate
     seg = seg.unsqueeze(0)
-    seg = seg.expand(3,seg.size(1),seg.size(2))
+    seg = seg.expand(3, seg.size(1), seg.size(2))
     seg = transforms.ToPILImage()(seg)
-    seg = seg.resize((H,W))
+    seg = seg.resize((H, W))
     seg = transforms.ToTensor()(seg)[0].squeeze(0)
     return seg
 
-def wct2(cF,sF):
+
+def wct2(cF, sF):
     cFSize = cF.size()
-    c_mean = torch.mean(cF,1) # c x (h x w)
+    c_mean = torch.mean(cF, 1)  # c x (h x w)
     c_mean = c_mean.unsqueeze(1).expand_as(cF)
     cF = cF - c_mean
 
-    contentConv = torch.mm(cF,cF.t()).div(cFSize[1]-1) + torch.eye(cFSize[0]).double()
-    c_u,c_e,c_v = torch.svd(contentConv,some=False)
+    contentConv = torch.mm(
+        cF, cF.t()).div(cFSize[1] - 1) + torch.eye(cFSize[0]).double()
+    c_u, c_e, c_v = torch.svd(contentConv, some=False)
 
     k_c = cFSize[0]
     for i in range(cFSize[0]):
@@ -68,10 +122,10 @@ def wct2(cF,sF):
             break
 
     sFSize = sF.size()
-    s_mean = torch.mean(sF,1)
+    s_mean = torch.mean(sF, 1)
     sF = sF - s_mean.unsqueeze(1).expand_as(sF)
-    styleConv = torch.mm(sF,sF.t()).div(sFSize[1]-1)
-    s_u,s_e,s_v = torch.svd(styleConv,some=False)
+    styleConv = torch.mm(sF, sF.t()).div(sFSize[1] - 1)
+    s_u, s_e, s_v = torch.svd(styleConv, some=False)
 
     k_s = sFSize[0]
     for i in range(sFSize[0]):
@@ -80,70 +134,76 @@ def wct2(cF,sF):
             break
 
     c_d = (c_e[0:k_c]).pow(-0.5)
-    step1 = torch.mm(c_v[:,0:k_c],torch.diag(c_d))
-    step2 = torch.mm(step1,(c_v[:,0:k_c].t()))
-    whiten_cF = torch.mm(step2,cF)
+    step1 = torch.mm(c_v[:, 0:k_c], torch.diag(c_d))
+    step2 = torch.mm(step1, (c_v[:, 0:k_c].t()))
+    whiten_cF = torch.mm(step2, cF)
 
     s_d = (s_e[0:k_s]).pow(0.5)
-    targetFeature = torch.mm(torch.mm(torch.mm(s_v[:,0:k_s],torch.diag(s_d)),(s_v[:,0:k_s].t())),whiten_cF)
+    targetFeature = torch.mm(
+        torch.mm(
+            torch.mm(s_v[:, 0:k_s], torch.diag(s_d)), (s_v[:, 0:k_s].t())),
+        whiten_cF)
     targetFeature = targetFeature + s_mean.unsqueeze(1).expand_as(targetFeature)
     return targetFeature
 
-def feature_wct(cF,sF,csF):
+
+def feature_wct(cF, sF, csF):
     cF = cF.double()
     sF = sF.double()
-    C,W,H = cF.size(0),cF.size(1),cF.size(2)
-    _,W1,H1 = sF.size(0),sF.size(1),sF.size(2)
-    cFView = cF.view(C,-1)
-    sFView = sF.view(C,-1)
+    C, W, H = cF.size(0), cF.size(1), cF.size(2)
+    _, W1, H1 = sF.size(0), sF.size(1), sF.size(2)
+    cFView = cF.view(C, -1)
+    sFView = sF.view(C, -1)
 
-    targetFeature = wct2(cFView,sFView)
+    targetFeature = wct2(cFView, sFView)
     targetFeature = targetFeature.view_as(cF)
     ccsF = args.alpha * targetFeature + (1.0 - args.alpha) * cF
     ccsF = ccsF.float().unsqueeze(0)
     csF.data.resize_(ccsF.size()).copy_(ccsF)
     return csF
 
-def styleTransfer(contentImg,styleImg,imname,csF):
-    e1,d1,e2,d2,e3,d3,e4,d4,e5,d5 = loadModel(args)
+
+def styleTransfer(contentImg, styleImg, imname, csF):
+    e1, d1, e2, d2, e3, d3, e4, d4, e5, d5 = loadModel(args)
 
     sF5 = e5(styleImg)
     cF5 = e5(contentImg)
     sF5 = sF5.data.cpu().squeeze(0)
     cF5 = cF5.data.cpu().squeeze(0)
-    csF5 = feature_wct(cF5,sF5,csF)
+    csF5 = feature_wct(cF5, sF5, csF)
     Im5 = d5(csF5)
 
     sF4 = e4(Im5)
     cF4 = e4(contentImg)
     sF4 = sF4.data.cpu().squeeze(0)
     cF4 = cF4.data.cpu().squeeze(0)
-    csF4 = feature_wct(cF4,sF4,csF)
+    csF4 = feature_wct(cF4, sF4, csF)
     Im4 = d4(csF4)
 
     sF3 = e3(styleImg)
     cF3 = e3(Im4)
     sF3 = sF3.data.cpu().squeeze(0)
     cF3 = cF3.data.cpu().squeeze(0)
-    csF3 = feature_wct(cF3,sF3,csF)
+    csF3 = feature_wct(cF3, sF3, csF)
     Im3 = d3(csF3)
 
-    sF2= e2(styleImg)
+    sF2 = e2(styleImg)
     cF2 = e2(Im3)
     sF2 = sF2.data.cpu().squeeze(0)
     cF2 = cF2.data.cpu().squeeze(0)
-    csF2 = feature_wct(cF2,sF2,csF)
+    csF2 = feature_wct(cF2, sF2, csF)
     Im2 = d2(csF2)
 
     sF1 = e1(styleImg)
     cF1 = e1(Im2)
     sF1 = sF1.data.cpu().squeeze(0)
     cF1 = cF1.data.cpu().squeeze(0)
-    csF1 = feature_wct(cF1,sF1,csF)
+    csF1 = feature_wct(cF1, sF1, csF)
     Im1 = d1(csF1)
     # save_image has this wired design to pad images with 4 pixels at default.
-    vutils.save_image(Im1.data.cpu().float(),os.path.join(args.outf,imname))
+    vutils.save_image(Im1.data.cpu().float(), os.path.join(args.outf, imname))
     return
+
 
 avgTime = 0
 cImg = torch.Tensor()
@@ -152,20 +212,22 @@ csF = torch.Tensor()
 cImg = Variable(cImg)
 sImg = Variable(sImg)
 csF = Variable(csF)
-if(args.cuda):
+if (args.cuda):
     cImg = cImg.cuda()
     sImg = sImg.cuda()
     csF = csF.cuda()
-for i,(contentImg,styleImg,imname) in enumerate(loader):
+
+for i, (contentImg, styleImg, imname) in enumerate(loader):
     imname = imname[0]
     print('Transferring ' + imname)
     cImg.data.resize_(contentImg.size()).copy_(contentImg)
     sImg.data.resize_(styleImg.size()).copy_(styleImg)
     start_time = time.time()
     # WCT Style Transfer
-    styleTransfer(cImg,sImg,imname,csF)
+    styleTransfer(cImg, sImg, imname, csF)
     end_time = time.time()
     print('Elapsed time is: %f' % (end_time - start_time))
     avgTime += (end_time - start_time)
 
-print('Processed %d images. Averaged time is %f' % ((i+1),avgTime/(i+1)))
+print('Processed %d images. Averaged time is %f' % ((i + 1),
+                                                    avgTime / (i + 1)))
